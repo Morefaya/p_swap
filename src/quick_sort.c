abbda76329b8cff482c2db34c12a1abdb128f3ac
Jean-charles CAZAKO <jcazako@e1r4p11.42.fr>
2016-08-13 20:09:17 +0200

    starting counting

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: jcazako <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/10 13:01:49 by jcazako           #+#    #+#             */
-/*   Updated: 2016/08/10 19:23:39 by jcazako          ###   ########.fr       */
+/*   Updated: 2016/08/13 19:34:28 by jcazako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,13 @@ int	quick_sort(t_list **lst_a, t_list **lst_b, int opt)
 		{
 			ret++;
 			push(lst_a, lst_b);
+			ft_putendl("P");
 		}
 		else
+		{
 			rotate(lst_a);
+			ft_putendl("R");
+		}
 		i++;
 	}
 	return (ret);
--- src/snd_step.c
+++ src/snd_step.c
@@ -6,7 +6,7 @@
 /*   By: jcazako <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/13 19:19:07 by jcazako           #+#    #+#             */
-/*   Updated: 2016/08/13 19:21:40 by jcazako          ###   ########.fr       */
+/*   Updated: 2016/08/13 20:06:40 by jcazako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,23 @@ void		snd_step(t_list **lst, int opt)
 	int		high;
 	int		low;
 
+	if (!lst)
+		return ;
 	high = get_highest(*lst);
 	low = get_lowest(*lst);
 	while (((check_asc_rot(*lst) != 1) && opt)
 		|| ((check_asc_rot(*lst) != -1) && !opt))
 	{
-		//print_lst(*lst);
-		//ft_putchar('\n');
 		value = (((t_pile*)(*lst)->content))->val;
 		value_next = (((t_pile*)(*lst)->next->content))->val;
 		if ((value > value_next && value != high && opt)
 			|| (value < value_next && value != low && !opt))
+		{
+			ft_putendl("Sa");
 			swap(lst);
+		}
 		rotate(lst);
+		ft_putendl("Ra");
 	}
 	turn_list(lst);
-	//print_lst(*lst);
-	//ft_putchar('\n');
 }
