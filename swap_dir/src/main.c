bdd71a87c515bff73bd6648d9760935e6021183f
Jean-charles CAZAKO <jcazako@e2r1p7.42.fr>
2016-08-20 20:14:12 +0200

    keep going on range_sort

--- swap_dir/src/main.c
+++ swap_dir/src/main.c
@@ -6,7 +6,7 @@
 /*   By: jcazako <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/08 20:43:49 by jcazako           #+#    #+#             */
-/*   Updated: 2016/08/20 17:39:33 by jcazako          ###   ########.fr       */
+/*   Updated: 2016/08/20 19:33:34 by jcazako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,20 +27,22 @@ int		main(int ac, char **av)
 	ft_putchar('\n');
 	quick_sort(&lst_a, &lst_b);
 
-	print_lst(lst_a);
-	print_lst(lst_b);
-	ft_putchar('\n');
+	//print_lst(lst_a);
+	//print_lst(lst_b);
+	//ft_putchar('\n');
 	jc_sort(&lst_a, 1);
-	ft_putchar('\n');
-	jc_sort(&lst_b, 0);
-	ft_putchar('\n');
+	//ft_putchar('\n');
+	jc_sort(&lst_b, 1);
+	//ft_putchar('\n');
 
 	print_lst(lst_a);
 	print_lst(lst_b);
 	ft_putchar('\n');
 
 	range_sort(&lst_a, 1);
+	range_sort(&lst_a, 1);
 	print_lst(lst_a);
+	print_lst(lst_b);
 	/*ft_putnbr(get_higher(lst_a, 9));
 	ft_putchar('\n');
 	ft_putnbr(get_lower(lst_b, 5));
--- swap_dir/src/range_sort.c
+++ swap_dir/src/range_sort.c
@@ -6,7 +6,7 @@
 /*   By: jcazako <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/20 13:28:27 by jcazako           #+#    #+#             */
-/*   Updated: 2016/08/20 17:44:54 by jcazako          ###   ########.fr       */
+/*   Updated: 2016/08/20 20:08:45 by jcazako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,25 @@ void	range_sort(t_list **lst, int asc)
 	int		lim_low;
 	int		lim_high;
 	int		higher;
-	//int		lower;
+	int		lower;
 	int		value;
+	//int		i;
 
 	lim_low = get_lowest(*lst);
 	lim_high = get_highest(*lst);
 	if (asc)
 	{
-		if (asc)
+		rotate(lst);
+		//i = 0;
+		while (check_asc_rot(*lst) != 1)
 		{
 			ft_putendl("a");
 			value = (((t_pile*)(*lst)->content))->val;
-			if (value == lim_low)
+			if (get_prev(*lst) == lim_low)
 			{
 				higher = get_higher(*lst, lim_low);
 				rotate(lst);
+				ft_putchar('@');
 				while ((((t_pile*)(*lst)->content))->val == higher)
 				{
 					ft_putendl("b");
@@ -47,10 +51,28 @@ void	range_sort(t_list **lst, int asc)
 					rotate(lst);
 				}
 			}
-			else if (value == lim_high)
+			else if ((((t_pile*)(*lst)->next->content))->val == lim_high)
 			{
-				ft_putchar('@');
+				lower = get_lower(*lst, lim_high);
+				rev_rotate(lst);
+				ft_putchar('d');
+				while ((((t_pile*)(*lst)->content))->val == lower)
+				{
+					ft_putchar('e');
+					lim_high = lower;
+					lower = get_lower(*lst, lim_high);
+					rev_rotate(lst);
+				}
+				while (get_prev(*lst) != lim_low)
+				{
+					ft_putchar('f');
+					rev_rotate(lst);
+					if (check_next(*lst))
+						swap(lst);
+				}
 			}
+			print_lst(*lst);
+			ft_putchar('\n');
 		}
 	}
 }
